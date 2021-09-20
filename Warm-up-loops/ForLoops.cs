001276e1eec8244698bf27f0b46ca90250707960
Adam Batson <tibatson@gmail.com>
2021-09-20 14:02:10 -0500

    Fix .gitignore conflict

---
+++ Warm-up-loops/ForLoops.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warm_up_loops
+{
+    public class ForLoop
+    {
+        /// <summary>
+        /// Return lowest number in array
+        /// </summary>
+        /// <param name="arr">System.Int32[]</param>
+        /// <returns>Returns lowest number in array.  If array is empty, returns null</returns>
+        public int? GetLowest(int[] arr)
+        {
+            if (arr.Length == 0) return null;
+
+            var number = arr[0];
+
+            for(var i = 1; i < arr.Length; i++)
+            {
+                if (number > arr[i]) number = arr[i];
+            }
+
+            return number;
+        }
+
+        /// <summary>
+        /// Return highest number in array
+        /// </summary>
+        /// <param name="arr">System.Int32[]</param>
+        /// <returns>Returns highest number in array.  If array is empty, returns null</returns>
+        public int? GetHighest(int[] arr)
+        {
+            if (arr.Length == 0) return null;
+
+            var number = arr[0];
+
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (number < arr[i]) number = arr[i];
+            }
+
+            return number;
+        }
+
+        /// <summary>
+        /// Return highest number in jagged array
+        /// </summary>
+        /// <param name="arr">System.Int32[][]</param>
+        /// <returns>Returns highest number in array.  If there are no numbers provided, returns null</returns>
+        public int? GetHighest(int[][] arr)
+        {
+            if (arr.Length == 0 || arr == null) return null;
+
+            int? number = null;
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                for (var j = 0; j < arr[i].Length; j++)
+                {
+                    if (number == null || number < arr[i][j]) number = arr[i][j];
+                }
+            }
+            return number;
+        }
+    }
+}
---
+++ Warm-up-loops_tests/ReverseAString_tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warm_up_loops_tests
+{
+    [TestClass]
+    public class ReverseString_tests
+    {
+       // public void 
+    }
+}
